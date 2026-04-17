5ae41133610f6a674fce8a8a697e433a90185b9f
Sakshi Bhansali <156528716+sabhansali@users.noreply.github.com>
2026-04-17 15:42:19 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+def main():
+    print("Simple CI Test Program")
+    
+    x = 5
+    y = 3
+    
+    result = add(x, y)
+    
+    print(f"Adding {x} and {y}")
+    print(f"Result: {result}")
+
+if __name__ == "__main__":
+    main()
